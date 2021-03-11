d4c8cdd676df339e9e0c8131e647b074a9320c14
karthi <karthi@exm.com>
2021-03-11 16:46:36 +0530

    lists with listView

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'models/image_model.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'widgets/image_list.dart';
 
 class App extends StatefulWidget {
   createState() {
@@ -11,18 +12,23 @@ class App extends StatefulWidget {
 
 class AppState extends State<App> {
   int counter = 0;
+  List<ImageModel> images = [];
 
   void fetchImage() async {
     counter++;
-    var response =
-        await http.get('http://jsonplaceholder.typicode.com/photos/$counter');
+    var response = await http.get('http://jsonplaceholder.typicode.com/photos/$counter');
     var imageModel = ImageModel.fromJson(json.decode(response.body));
     // print(imageModel);
+
+    setState(() {
+      images.add(imageModel);
+    });
   }
 
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
+        body: ImageList(images),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
           onPressed: fetchImage,
