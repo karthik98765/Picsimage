6ae149f8cd67dbb8d242ae9774ddd131104d0942
karthi <karthi@exm.com>
2021-03-13 13:44:03 +0530

    adding border

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -10,8 +10,15 @@ class ImageList extends StatelessWidget {
     return ListView.builder(
       itemCount: images.length,
       itemBuilder: (context, int index) {
-        
-      }
+        return Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+          ),
+          padding: EdgeInsets.all(20.0),
+          margin: EdgeInsets.all(20.0),
+          child: Image.network(images[index].url),
+        );
+      },
     );
   }
 }
